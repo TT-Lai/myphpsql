1bf535d2102aa3ba3fbcdc486f3c7a61b9f59347
TT-Lai <107803464+TT-Lai@users.noreply.github.com>
2023-01-01 22:03:02 +0800

    pythonpo

--- python-20221206/pythonpo/datatype.py
+++ python-20221206/pythonpo/datatype.py
@@ -1,2 +1,12 @@
-if 5>2:
-    print ("five is greater than two!")
+#if 5>2:
+#    print ("five is greater than two!")
+x=3
+print(x)
+x=True
+print(x)
+x="Hello"
+print(x)
+x={2,3,4,5}
+print(x)
+x=[2,3,45,3,6]
+print(x)
